b4b8d9328bfe10437c4a2c38916b8d49696e75a6
Atefe <aa.asgariyan12@gmail.com>
2024-06-13 11:05:25 +0330

    round the rating calculation

--- resources/js/Components/Products/Product.jsx
+++ resources/js/Components/Products/Product.jsx
@@ -4,14 +4,13 @@ import Features from "@/Components/Products/Features.jsx";
 import Comments from "@/Components/Products/Comments.jsx";
 import Price from "@/Components/Products/Price.jsx";
 import PrimaryButton from "@/Components/PrimaryButton.jsx";
-import {useRef, useState} from "react";
+import {useRef} from "react";
 import html2pdf from "html2pdf.js";
 
 export default function Product({data}) {
-
-    const [isLoading, setIsLoading] = useState(false);
-    const [productData, setProductData] = useState();
-    const [fetchError, setFetchError] = useState(null)
+    //
+    // const [isLoading, setIsLoading] = useState(false);
+    // const [fetchError, setFetchError] = useState(null)
 
     const productId = data.product.id;
     const pdfRef = useRef(null);
@@ -68,7 +67,7 @@ export default function Product({data}) {
                         className="my-6  items-start gap-6 overflow-hidden rounded-lg bg-white p-20 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-200 dark:ring-zinc-100 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                     >
                         <div>{data.product.title_fa}</div>
-                        <div>{Math.round(data.product.rating.rate * 5 / 100)} ⭐</div>
+                        <div>{Math.round(data.product.rating.rate * 5 / 100 * 10) / 10} ⭐</div>
                         <div>{data.product.rating.count} نظر</div>
                         {(data.product.default_variant?.price || data.product.variants.length > 0) ?
                             <Price priceData={data.product.default_variant.price || data.product.variants[0].price}/>
